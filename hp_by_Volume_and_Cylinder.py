94869dfebd8fa21a12ea184b869a6db61e9acbb9
Alireza Shirmohammad <alirezashirmohammad55@gmail.com>
2022-07-18 00:17:10 +0430

    Initial 🚀

---
+++ hp_by_Volume_and_Cylinder.py
@@ -0,0 +1,17 @@
+import pandas as pn
+from sklearn import linear_model
+
+df = pn.read_csv('cars.csv')
+
+X = df[['Volume', 'Cylinder']]
+y = df['hp']
+
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
+
+v = float(input('Insert Volume of the engine: '))
+c = float(input('Insert number of Cylinders: '))
+
+predicthp = regr.predict([[v, c]])
+
+print(predicthp)
